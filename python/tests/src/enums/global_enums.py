c031b6b2a49c01ba9e70d9488ff505d0f4a84387
Nickolit <kbaht2006@gmail.com>
2023-04-14 13:27:58 +0300

    Refactor update

--- python/tests/src/enums/global_enums.py
+++ python/tests/src/enums/global_enums.py
@@ -4,3 +4,4 @@ from enum import Enum
 class GlobalErrorMessages(Enum):
     WRONG_STATUS_CODE = 'Полученный статус-код страницы не равен ожидаемому'
     WRONG_ELEMENT_COUNT = 'Полученное количество элементов не соответствует ожидаемому'
+    WRONG_URL_STATUS = 'Полученный статус-код не равен ожидаемому'
--- python/tests/test_sk_internall.py
+++ python/tests/test_sk_internall.py
@@ -2,10 +2,10 @@ import pytest
 import requests
 import allure
 
+from src.enums.global_enums import GlobalErrorMessages
 
 with open('_internal_links.txt', 'r') as f:
     internal_urls = f.read().splitlines()
-
 
 @allure.feature('Check url')
 @allure.story('Запрашиваем статус-код ссылки')
@@ -14,4 +14,4 @@ def test_getting_posts(internal_url):
     response = requests.get(internal_url)
 
     with allure.step("Запрос отправлен. Сверяем ответ url на статус-код 200"):
-        assert response.status_code == 200, "Полученное количество элементов не соответствует ожидаемому"
+        assert response.status_code == 200, GlobalErrorMessages.WRONG_URL_STATUS.value "Полученное количество элементов не соответствует ожидаемому"
